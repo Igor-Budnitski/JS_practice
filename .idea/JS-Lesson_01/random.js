24979191314dfd631347b20271128a9b545b7b26
Igor Budnitski <igor.budnitski@gmail.com>
2025-09-20 19:27:38 +0300

    Random

--- .idea/JS-Lesson_01/random.js
+++ .idea/JS-Lesson_01/random.js
@@ -7,4 +7,20 @@ for (let i = 0; i < 100; i++) {
     arr.push(Math.ceil(Math.random() * (6)));
 }
 
-console.log(arr);
+// console.log(arr);
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+console.log(`Rol dice 1 to 6: ${rollDice()}`);
+
