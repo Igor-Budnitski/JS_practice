540aa4289235cf4a8e43ab56b877d105c203fb1e
Igor Budnitski <igor.budnitski@gmail.com>
2025-09-20 18:31:23 +0300

    Random

--- .idea/JS-Lesson_01/random.js
+++ .idea/JS-Lesson_01/random.js
@@ -1,4 +1,10 @@
 
-for (let i = 0; i < 10; i++) {
+/*for (let i = 0; i < 10; i++) {
     console.log(`Random number: ${Math.floor(Math.random() * (10))}`)
+}*/
+let arr = [];
+for (let i = 0; i < 100; i++) {
+    arr.push(Math.ceil(Math.random() * (6)));
 }
+
+console.log(arr);
