a315079d4f11d7f8601200a0475076c9cad6932a
Igor Budnitski <igor.budnitski@gmail.com>
2025-10-08 19:52:03 +0300

    home work 12

--- .idea/JS-Lesson_12/Homework 12 lesson/server.js
+++ .idea/JS-Lesson_12/Homework 12 lesson/server.js
@@ -1,19 +1,33 @@
 const http = require('http');
 const fs = require('fs');
 
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) reject('Rejected Error', err)
+            else resolve(data);
+        });
+    })
+}
 
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/home'){
-        res.write("Home page!");
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/home') {
+        const data = await readFile('pages/home.html');
+        res.write(data);
         res.end();
-        return;
     }
 
-    if (req.url === '/about'){
-        res.write('About page!')
+    if (req.url === '/about') {
+        await delay(5000);
+        res.write("ABOUT COURSE PROMISE!");
         res.end();
-        return;
     }
 })
 
